fedd5fbb28ea4aafe5abadb64927f83f1714ac72
khshakilahamed <khshakil.ahamed18@gmail.com>
2022-03-09 02:36:36 +0600

    delete order API added

--- index.js
+++ index.js
@@ -60,7 +60,14 @@ async function run(){
         });
 
         // DELETE API
-        
+        app.delete('/orders/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            console.log(result)
+            res.json(result)
+        })
+
     }
     finally{
         // await client.close();
